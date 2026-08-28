2bc01ae0eef5dd26fc37a7a030229a781e46a575
Laud Tetteh <kobbykurtx@gmail.com>
2026-08-28 20:53:12 +0000

    chore: import Vitals reading module into NurseAgent module in order to inject vitalsReadingService

--- apps/backend/src/modules/nurse-agent/nurse-agent.module.ts
+++ apps/backend/src/modules/nurse-agent/nurse-agent.module.ts
@@ -3,9 +3,10 @@ import { NurseAgentController } from './nurse-agent.controller';
 import { NurseAgentService } from './nurse-agent.service';
 import { NurseAgentToolsService } from './nurse-agent-tools.service';
 import { PatientModule } from '../patient/patient.module';
+import { VitalReadingModule } from '../vital-reading/vital-reading.module';
 
 @Module({
-  imports: [PatientModule],
+  imports: [PatientModule, VitalReadingModule],
   controllers: [NurseAgentController],
   providers: [NurseAgentService, NurseAgentToolsService],
 })
